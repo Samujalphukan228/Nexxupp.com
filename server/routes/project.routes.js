f764069865b41049a49322fc946d62cda356a1e4
Your Name <your.email@example.com>
2025-10-20 00:32:32 +0530

    14

--- server/routes/project.routes.js
+++ server/routes/project.routes.js
@@ -5,16 +5,12 @@ import { adminAuth } from '../middleware/admin.middleware.js'
 
 const projectRouter = express.Router()
 
-<<<<<<< HEAD
+
 
 projectRouter.post('/add', adminAuth, upload.single('image'), createProject);
-projectRouter.post('/remove', adminAuth, removeProjects);
-projectRouter.post('/all', listProjects)
-=======
-projectRouter.post('/add', adminAuth, upload.single('image'), createProject);
 projectRouter.post('/all', listProjects)
 projectRouter.post('/remove', adminAuth, removeProjects);
->>>>>>> 3158a1380b139d6989d8d7aaa90af446595502cd
+
 projectRouter.post('/single/', singleProject)
 
 export default projectRouter
